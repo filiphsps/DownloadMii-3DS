fed29fa78ef4e47c33ba25f2323bc338426a5a50
Filiph Sandström <filfat@hotmail.se>
2014-11-21 13:29:31 +0000

    Added compile.bat

---
+++ compile.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+REM This file is used by the build server!
+make clean
+make
+REM Create cia file
+REM makerom.exe -elf DownloadMii.elf -rsf DownloadMii.rsf -icon DownloadMii.icr -banner DownloadMii.bnr -o homebrew.cxi
+REM makerom.exe -f cia -content DownloadMii.cxi:0:0 -o DownloadMii.cia
