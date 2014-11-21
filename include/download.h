cf33f1cea48ac7b491757b33134b3b856992cba3
Filiph Sandström <filfat@hotmail.se>
2014-11-21 14:13:33 +0000

    made buffer smaller.

--- include/download.h
+++ include/download.h
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <netdb.h>
 #define SERVER_HOSTNAME "downloadmii.filfatstudios.com"
-#define BUFFER_SIZE 63*10000
+#define BUFFER_SIZE 63*1000
 
 using namespace std;
 /* Public functions */
