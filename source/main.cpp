5e7a1bb64aa791b7bdd63d4f1259918710e79282
Filiph Sandström <filfat@hotmail.se>
2014-11-22 10:43:31 +0100

    Update main.cpp
    Making the app bare bones

--- source/main.cpp
+++ source/main.cpp
@@ -11,7 +11,7 @@
 #include "gui.h"
 #include "input.h"
 //#include "json.h"
-#include "download.h"
+//#include "download.h"
 
 using namespace std;
 //using namespace picojson;
@@ -28,45 +28,31 @@ int main(int argc, char** argv)
 	hidInit(NULL);
 	gfxInit();
 	initGUI();
-	Result r = networkInit();
-	if(r != 0){
+	//Result r = networkInit();
+	/*if(r != 0){
 		//Error,
 		//ToDo: Set application in offline mode
-	}
+	}*/
 	//gfxSet3D(true); //uncomment if using stereoscopic 3D
 	
-	APP_STATUS status;
+	//APP_STATUS status;
 	
 	//Test
-	char* url = "http://downloadmii.filfatstudios.com/applications.json";
-	char* jsonSS = downloadFile(url);
-	if(jsonSS != NULL){
+	//char* url = "http://downloadmii.filfatstudios.com/applications.json";
+	//char* jsonSS = downloadFile(url);
+	/*if(jsonSS != NULL){
 		debug(jsonSS);
 	}
 	else{
 		debug("");
-	}
+	}*/
 	
 	/* Main loop */
-	while ((status = aptGetStatus()) != APP_EXITING)
+	while (aptMainLoop())
 	{
-		if (status == APP_RUNNING)
-		{
-			//If the app is currently in the forground running, execute the program.
-			MainLoop();
-			if (Input.Start == true){
-				break; //break in order to return to hbmenu
-			}
-		}
-		else if (status == APP_SUSPENDING)
-		{
-			//If the app is currently suspended in the background, return to the home menu.
-			break;
-		}
-		else if (status == APP_SLEEPMODE)
-		{
-			//If the app is currently suspended in sleep mode, wait.
-			aptWaitStatusEvent();
+		MainLoop();
+		if (Input.Start == true){
+			break; //break in order to return to hbmenu
 		}
 	}
 
