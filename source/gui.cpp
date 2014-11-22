6a61c5cbd63a8ebf5fdbbae4c2a44a735e53a4b4
Filiph Sandström <filfat@hotmail.se>
2014-11-22 18:23:52 +0100

    Update gui.cpp
    Apparently the screen is sideways. Huh?

--- source/gui.cpp
+++ source/gui.cpp
@@ -14,6 +14,7 @@
 using namespace std;
 
 /*
+(They should be inverted for some reason)
 TopScreen: w400 h240
 BottomScreen: w320 h240
 */
@@ -33,7 +34,7 @@ void renderGUI(){
 	whiteBG();
 
 	/* UI */
-	navBar();
+	//navBar(); //Might be the problem we are facing.
 	if(DEBUG == true){
 		//renderDebug();
 	}
@@ -51,24 +52,24 @@ void debug(char* text){
 void renderDebug(){
 	char buffer[110];
 	sprintf(buffer, debugStr);
-	drawString(buffer, 5, 200, 178, 63, 255, screenTopLeft, GFX_TOP); 
- 	drawString(buffer, 5, 200, 178, 63, 255,  screenTopRight, GFX_TOP);
+	drawString(buffer, 28, 200, 178, 63, 255, screenTopLeft, GFX_TOP); 
+ 	drawString(buffer, 28, 200, 178, 63, 255,  screenTopRight, GFX_TOP);
 }
 void navBar(){ //ToDo: Change name to "topNavBar()"
-	drawRect(0,0,400,60, 33,150,243, screenTopLeft);
-	drawRect(0,0,400,60, 33,150,243, screenTopRight);
+	drawRect(0,0,60,400, 33,150,243, screenTopLeft);
+	drawRect(0,0,60,400, 33,150,243, screenTopRight);
 	
 	char buffer[110];
 	sprintf(buffer, APPLICATION_NAME);
-	drawString(buffer, 5, 28, 255, 255, 255, screenTopLeft, GFX_TOP); 
- 	drawString(buffer, 5, 28, 255, 255, 255, screenTopRight, GFX_TOP);
+	drawString(buffer, 28, 5, 255, 255, 255, screenTopLeft, GFX_TOP); 
+ 	drawString(buffer, 28, 5, 255, 255, 255, screenTopRight, GFX_TOP);
 	
 	sprintf(buffer, "            v1.0");
-	drawString(buffer, 5, 28, 255, 255, 255, screenTopLeft, GFX_TOP); 
- 	drawString(buffer, 5, 28, 255, 255, 255, screenTopRight, GFX_TOP);
+	drawString(buffer, 28, 5, 255, 255, 255, screenTopLeft, GFX_TOP); 
+ 	drawString(buffer, 28, 5, 255, 255, 255, screenTopRight, GFX_TOP);
 }
 void whiteBG(){
-	drawFillRect( 0, 0, 400, 240, 255, 255, 255, screenTopLeft);
-	drawFillRect( 0, 0, 400, 240, 255, 255, 255, screenTopRight);
-	drawFillRect( 0, 0, 400, 240, 255, 255, 255, screenBottom);
+	drawFillRect( 0, 0, 240, 400, 255, 255, 255, screenTopLeft);
+	drawFillRect( 0, 0, 240, 400, 255, 255, 255, screenTopRight);
+	drawFillRect( 0, 0, 240, 400, 255, 255, 255, screenBottom);
 }
