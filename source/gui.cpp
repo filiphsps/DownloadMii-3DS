865346686bb0aea2d713862d4f634d63d94878db
Filiph Sandström <filfat@hotmail.se>
2014-11-22 23:15:12 +0100

    might be working

--- source/gui.cpp
+++ source/gui.cpp
@@ -27,28 +27,23 @@ void renderGUI(){
 	screenBottom = gfxGetFramebuffer(GFX_BOTTOM, GFX_LEFT, NULL, NULL); 
  	screenTopLeft = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL); 
  	screenTopRight = gfxGetFramebuffer(GFX_TOP, GFX_RIGHT, NULL, NULL);
-	for(int x = 0; x <= 1; x++){
-		/* Clear Screen */
-		clearScreen(screenTopLeft, GFX_TOP); 
-		clearScreen(screenTopRight, GFX_TOP);
+	/* Clear Screen */
+	clearScreen(screenTopLeft, GFX_TOP); 
+	clearScreen(screenTopRight, GFX_TOP);
 
-		/* Background */
-		background();
+	/* Background */
+	background();
 
-		/* UI */
-		
-		/* DEBUG */
-		if(DEBUG == true){
-			renderDebug();
-		}
-		
-		/* Buffers */
-		screenBottom = gfxGetFramebuffer(GFX_BOTTOM, GFX_LEFT, NULL, NULL); 
-		screenTopLeft = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL); 
-		screenTopRight = gfxGetFramebuffer(GFX_TOP, GFX_RIGHT, NULL, NULL); 
-		gfxFlushBuffers(); 
-		gfxSwapBuffers(); 
+	/* UI */
+	
+	/* DEBUG */
+	if(DEBUG == true){
+		renderDebug();
 	}
+	
+	/* Buffers */
+	gfxFlushBuffers(); 
+	gfxSwapBuffers(); 
 }
 /* UIs */
 void renderDebug(){
