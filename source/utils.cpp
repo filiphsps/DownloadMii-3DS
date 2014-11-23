725c4b65c8e7445da24adff79c80501f6db45e10
ichfly <Meier.Hanz@gmx.net>
2014-11-23 15:32:35 +0100

    fixed of by one in drawPixel

--- source/utils.cpp
+++ source/utils.cpp
@@ -7,6 +7,59 @@
 #include <3ds.h>
 #include "utils.h"
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+
+const int commandPort=5000;
+
 using namespace std;
 
-//Todo
+int sock = 0;
+
+unsigned long htonl(unsigned long v)
+{
+	u8* v2=(u8*)&v;
+	return (v2[0]<<24)|(v2[1]<<16)|(v2[2]<<8)|(v2[3]);
+}
+
+unsigned short htons(unsigned short v)
+{
+	u8* v2=(u8*)&v;
+	return (v2[0]<<8)|(v2[1]);
+}
+
+
+void sock_debuginit()
+{
+    SOC_Initialize((u32*)memalign(0x1000, 0x100000), 0x100000);
+
+    int listenfd;
+    struct sockaddr_in serv_addr;
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(commandPort);
+
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+    listen(listenfd, 10);
+
+    sock = accept(listenfd, (struct sockaddr*)NULL, NULL);
+    closesocket(listenfd);
+}
+void sock_debugsendstr(char* mes)
+{
+    send(sock, mes, strlen(mes), 0);
+}
+void sock_debugwait()
+{
+    char d[] = {0x01, 0x01};
+    send(sock, d, 2, 0);
+    while (d[0] != 'E' && d[1] != 'X')
+		recv(sock, d, 0x2, 0); //just a wait
+}
