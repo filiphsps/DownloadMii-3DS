f7576f0f19387c929255f935ec367ff8b24c7a51
Filiph Sandström <filfat@hotmail.se>
2014-11-23 23:33:23 +0100

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,3 +1,7 @@
 #pragma once
 
-//ToDo
+unsigned long htonl(unsigned long v);
+unsigned short htons(unsigned short v);
+void sock_debuginit();
+void sock_debugsendstr(char* mes);
+void sock_debugwait();
