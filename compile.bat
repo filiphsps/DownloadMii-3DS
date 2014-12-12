24d7e578e0e0bd9b29fbcb35bceddd86c117be0f
Joostin <JoostinOnline@gmail.com>
2014-12-12 02:32:55 -0600

    Removed all the extra noise when compiling so only warnings/errors show up.

--- compile.bat
+++ compile.bat
@@ -1,7 +1,9 @@
 @ECHO OFF
 REM This file is used by the build server!
 make clean
-make
+echo build ...
+:: Using >NUL hides everything but the errors/warnings so you can spot them better
+make>NUL
 pause
 REM Create cia file
 REM makerom.exe -elf DownloadMii.elf -rsf DownloadMii.rsf -icon DownloadMii.icr -banner DownloadMii.bnr -o homebrew.cxi
