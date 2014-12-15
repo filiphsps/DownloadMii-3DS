86ea8fcc69aada1f02b52d0327a7fd5c81cb2eec
Filiph Sandström <filfat@hotmail.se>
2014-12-15 19:30:13 +0100

    Updated splash

--- source/splash.cpp
+++ source/splash.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 void doSplash(){
 	char buffer[110];
-	sprintf(buffer, "DownloadMii Build %u", 0/*(unsigned long) &__BUILD_NUMBER*/);
+	sprintf(buffer, "DownloadMii Build: %s\n", __DATE__);
 	for(int x = 0;x <= 1; x++){
 		screenTopLeft = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL); 
 		screenTopRight = gfxGetFramebuffer(GFX_TOP, GFX_RIGHT, NULL, NULL);
